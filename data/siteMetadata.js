2b9ec348b3e52f2b0cd67da50dfe6d2e7ba8a32b
raphourbe <raphael.becanne@pm.me>
2022-10-19 18:38:55 +0200

    Resolve issues with blog files

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,8 +5,8 @@ const siteMetadata = {
   description: 'A blog describing the problems I had to take care of as a CTO in a SME.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://blog-lyart-eta.vercel.app',
+  siteRepo: 'https://github.com/raphourbe/blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
