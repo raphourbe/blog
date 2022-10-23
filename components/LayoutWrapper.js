1ec40bf1f5d1e78d4c0fd90b65d4ae3641ee6d99
raphourbe <raphael.becanne@pm.me>
2022-10-23 19:41:38 +0200

    Add: rphl.dev

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,6 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+import Logo from '../data/logo'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
@@ -15,7 +15,7 @@ const LayoutWrapper = ({ children }) => {
           <div>
             <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
-                <div className="mr-3">
+                <div className="stroke-cyan mr-3">
                   <Logo />
                 </div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
