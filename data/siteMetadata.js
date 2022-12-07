c92422a1adfb22f1cd1f0114e4a89f26ee7d8ce4
raphourbe <37442071+raphourbe@users.noreply.github.com>
2022-12-07 11:48:28 +0100

    Update siteMetadata.js
    Add vercel analytics

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -23,6 +23,7 @@ const siteMetadata = {
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
     posthogAnalyticsId: '', // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
+    vercelAnalytics: true,
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
