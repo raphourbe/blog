d49a51c976af098380d24f9707a41a74a27ea18b
raphourbe <raphael.becanne@pm.me>
2023-08-07 21:24:44 +0200

    Update: blog v2

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,3 +1,4 @@
+/** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
   title: 'Adventures of a CTO',
   author: 'Raphaël Becanne',
@@ -61,21 +62,23 @@ const siteMetadata = {
       // example: https://giscus.app/themes/custom_example.css
       themeURL: '',
     },
-    utterancesConfig: {
-      // Visit the link below, and follow the steps in the 'configuration' section
-      // https://utteranc.es/
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
-      issueTerm: '', // supported options: pathname, url, title
-      label: '', // label (optional): Comment 💬
-      // theme example: github-light, github-dark, preferred-color-scheme
-      // github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light
-      theme: '',
-      // theme when dark mode
-      darkTheme: '',
-    },
     disqusConfig: {
       // https://help.disqus.com/en/articles/1717111-what-s-a-shortname
       shortname: process.env.NEXT_PUBLIC_DISQUS_SHORTNAME,
+    },
+    search: {
+      provider: 'kbar', // kbar or algolia
+      kbarConfig: {
+        searchDocumentsPath: 'search.json', // path to load documents to search
+      },
+      // provider: 'algolia',
+      // algoliaConfig: {
+      //   // The application ID provided by Algolia
+      //   appId: 'R2IYF7ETH7',
+      //   // Public API key: it is safe to commit it
+      //   apiKey: '599cec31baffa4868cae4e79f180729b',
+      //   indexName: 'docsearch',
+      // },
     },
   },
 }
