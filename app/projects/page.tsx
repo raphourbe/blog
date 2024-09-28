85e392aa66647befb69a5fa89e01200e353fba3e
raphourbe <raphael.becanne@pm.me>
2024-09-28 14:06:23 +0200

    Rm: Card in project

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -18,7 +18,7 @@ export default function Projects() {
         </div>
         <div className="container py-12">
           <div className="-m-4 flex flex-wrap">
-            {projectsData.map((d) => (
+            {/* {projectsData.map((d) => (
               <Card
                 key={d.title}
                 title={d.title}
@@ -26,7 +26,7 @@ export default function Projects() {
                 imgSrc={d.imgSrc}
                 href={d.href}
               />
-            ))}
+            ))} */}
           </div>
         </div>
       </div>
